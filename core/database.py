f3dc66569f92114684f58cc4658c640930d735d6
Wiktoria Pilch <wiktoria.pilch01@gmail.com>
2026-06-28 13:47:41 +0200

    feat: setup alembic and creation of subscription table

--- core/database.py
+++ core/database.py
@@ -5,3 +5,10 @@ from core.config import settings
 engine = create_engine(settings.DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
