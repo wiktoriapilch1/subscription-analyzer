a8b0478f0acba36c5d3469bcff2fca87f722a803
Wiktoria Pilch <wiktoria.pilch01@gmail.com>
2026-06-28 14:38:49 +0200

    feat: add GET /subcriptions and GET /subscriptions/{subscriptions_id} endpoints

--- subscriptions/router.py
+++ subscriptions/router.py
@@ -1,9 +1,13 @@
-from subscriptions.schemas import SubscriptionCreate
-from fastapi import Depends
-from sqlalchemy.orm import Session
+from fastapi import Depends, HTTPException
 from fastapi import APIRouter
-from core.database import get_db
+
+from sqlalchemy.orm import Session
+from sqlalchemy import select
+
+from subscriptions.schemas import SubscriptionCreate
 from subscriptions.models import Subscription
+
+from core.database import get_db
 
 router = APIRouter()
 
@@ -14,3 +18,16 @@ def create_subscription(newSubscription: SubscriptionCreate, session: Session = 
     session.commit()
     session.refresh(db_subscription)
     return db_subscription
+
+@router.get('/subscriptions')
+def get_subscriptin(session: Session = Depends(get_db)):
+    stmt = select(Subscription)
+    subsriptions = session.execute(stmt).scalars().all()
+    return subsriptions
+
+@router.get('/subscriptions/{subscription_id}')
+def get_subscription_with_id(subscription_id: int, session: Session = Depends(get_db)):
+    subscription = session.get(Subscription, subscription_id)
+    if subscription is None:
+        raise HTTPException(status_code=404, detail="Subscription not found")
+    return subscription
